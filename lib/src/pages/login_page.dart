13a2b2bada4cc871db345bb4eb5fa7a2560160e5
Franz Aruni <amrfranz@gmail.com>
2020-04-30 23:09:07 -0400

    Inicio de Login

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _crearFondo(context)
+        ],
+      )
+    );
+  }
+
+  Widget _crearFondo(BuildContext context){
+    final size = MediaQuery.of(context).size;
+    final fondoAzul =  Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color> [
+            Color.fromRGBO(66, 135, 245, 1.0),
+            Color.fromRGBO(98, 155, 245, 1.0)
+          ]
+        )
+      ),
+    );
+
+    final circulo = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        color: Color.fromRGBO(255, 255, 255, 0.05)
+      ),
+    );
+
+    return Stack(
+      children: <Widget>[
+        fondoAzul,
+        Positioned( top: 90.0, left: 30.0, child: circulo),
+        Positioned( top: -40.0, right: -30.0, child: circulo),
+        Positioned( bottom: -50.0, right: -10.0, child: circulo),
+        Positioned( bottom: 120.0, right: 20.0, child: circulo),
+        Positioned( bottom: -50.0, left: -20.0, child: circulo),
+      ],
+    );
+  }
+}
