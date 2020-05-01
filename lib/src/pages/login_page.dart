0c0d9e79878a147dae9ca2069791f3ca88bd377f
Franz Aruni <amrfranz@gmail.com>
2020-04-30 23:58:30 -0400

    Diseño login parte 1

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -7,9 +7,37 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          _crearFondo(context)
+          _crearFondo(context),
+          _loginForm(context)
         ],
       )
+    );
+  }
+
+  Widget _loginForm( BuildContext context ){
+    final size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SafeArea(
+            child: Container(
+              height: 180.0,
+            )
+          ),
+          Container(
+            width: size.width * 0.8,
+            height: size.height * 0.3,
+            decoration: BoxDecoration(
+              color: Colors.white
+            ),
+            child: Column(
+              children: <Widget>[
+                Text('Ingreso', style: TextStyle(fontSize: 20.0),)
+              ]
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -21,8 +49,8 @@ class LoginPage extends StatelessWidget {
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: <Color> [
-            Color.fromRGBO(66, 135, 245, 1.0),
-            Color.fromRGBO(98, 155, 245, 1.0)
+            Color.fromRGBO(0, 154, 221, 1.0),
+            Color.fromRGBO(0, 168, 227, 1.0)
           ]
         )
       ),
@@ -45,6 +73,20 @@ class LoginPage extends StatelessWidget {
         Positioned( bottom: -50.0, right: -10.0, child: circulo),
         Positioned( bottom: 120.0, right: 20.0, child: circulo),
         Positioned( bottom: -50.0, left: -20.0, child: circulo),
+
+        Container(
+          padding: EdgeInsets.only(top: 60.0),
+          child: Column(
+            children: <Widget>[
+              Image(
+                width: 120.0,
+                height: 120.0,
+                image: AssetImage('assets/logos/lafar.png'),
+              ),
+              SizedBox(width: double.infinity, height: 10.0,)
+            ],
+          ),
+        )
       ],
     );
   }
