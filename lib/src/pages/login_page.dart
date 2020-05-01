4426e692cbda1eb0678e8a4bf7a73e5dddffb44e
severo cruz <severo_86@hotmail.com>
2020-05-01 10:52:36 -0400

    boton y camposs de texto login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -26,21 +26,82 @@ class LoginPage extends StatelessWidget {
           ),
           Container(
             width: size.width * 0.8,
-            height: size.height * 0.3,
+            margin: EdgeInsets.symmetric(vertical:30.0),
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            //height: size.height * 0.3,
             decoration: BoxDecoration(
-              color: Colors.white
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 3.0,
+                  offset: Offset(0.0,5.0),
+                  spreadRadius: 3.0
+                )
+              ]
             ),
             child: Column(
               children: <Widget>[
-                Text('Ingreso', style: TextStyle(fontSize: 20.0),)
+                Text('Ingreso', style: TextStyle(fontSize: 20.0),),
+                SizedBox(height:60.0),
+                _crearEmail(),
+                SizedBox(height:30.0),
+                _crearPassword(),
+                SizedBox(height:30.0),
+                _crearBoton()
               ]
             ),
-          )
+          ),
+          Text('¿olvidó la contraseña?'),
+          SizedBox(height:100.0)
+          
         ],
       ),
     );
   }
+  Widget _crearEmail(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal:20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email,color: Colors.deepPurple,),
+          hintText: 'lmorales',
+          labelText: 'Nombre de Usuario'
+        ),
+      ),
+    );
+  }
+   Widget _crearPassword(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal:20.0),
+      child: TextField(
 
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock_outline ,color: Colors.deepPurple,),
+          labelText: 'Contraseña'
+        ),
+      ),
+    );
+  }
+  Widget _crearBoton(){
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0,vertical:15.0),
+        child: Text('Ingresar'),
+        ),
+        shape:RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0) 
+          ),
+          elevation: 0.0,
+          color: Color.fromRGBO(0, 154, 221, 1.0) ,
+          textColor: Colors.white,
+          
+        onPressed: (){},
+    );
+  }
   Widget _crearFondo(BuildContext context){
     final size = MediaQuery.of(context).size;
     final fondoAzul =  Container(
